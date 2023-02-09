5cac2532db3d011d38205689982c1843f34f0592
hassanawan02 <112867584+hassanawan02@users.noreply.github.com>
2023-02-09 15:37:17 +0100

    Delete quiz directory

--- quiz/QuizUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fuawan_oblig1.quiz
-
-import com.example.fuawan_oblig1.quiz.data.Question
-
-data class QuizUiState(val questions : List<Question>){
-    val riktigSvar = 0
-    var currentQuestionIndex = 0
-}
--- quiz/data/Question.kt
+++
@@ -1,3 +0,0 @@
-package com.example.fuawan_oblig1.quiz.data
-
-data class Question(val text: String, val svar: Boolean)
